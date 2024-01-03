2a7ba8eb6a14f074ea68e5f3e39e296e254366fb
denniskioko <kiokodennis6990@gmail.com>
2024-01-03 08:58:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(arrayOfObjects) {
+    const win = arrayOfObjects.find(obj => obj.result === "W" );
+    // wining record
+    return win ? win.year : undefined;
+}
